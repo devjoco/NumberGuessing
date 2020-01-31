815d0677b9c8a3e6cdedd2a62a8f20c0a2388d0d
devjoco <devonjoconnor@gmail.com>
2020-01-31 18:52:24 -0500

    Difficulty added

--- guess.py
+++ guess.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 import random
 
-lives = 3
-number = random.randint(1, 10)
+# Setup
+## Determine Difficulty
+## Easy   --> 5 lives, number ϵ [1,10]
+## Med    --> 4 lives, number ϵ [1,30]
+## Hard   --> 3 lives, number ϵ [1,50]
+## Custom --> Choice lives,  [Choice, Choice]
+invalidMode = True
+while invalidMode:
+    mode = input("Select your difficulty: [Easy, Med, Hard] ").strip().lower()
+    if mode in ["easy", "med", "hard",
+                "eas", "me", "har",
+                "ea", "m", "ha",
+                "e", "h",
+                "1", "2", "3"
+                ]:
+        invalidMode = False
+    else:
+        print(f"{mode.capitalize()} is not a valid difficulty")
 
-guess = int(input(f"I'm thinking of a number from 1 to 10, can you guess it in {lives} tries? "))
+likelyEasy = mode == "easy" or mode == "eas" or mode == "ea" or mode == "e" or mode == "1"
+likelyMed  = mode == "med" or mode == "me" or mode == "m" or mode == "2"
+likelyHard = mode == "hard" or mode == "har" or mode == "ha" or mode == "h" or mode == "3"
+lives  =  3 if likelyHard else ( 4 if likelyMed else  5)
+upper  = 50 if likelyHard else (30 if likelyMed else 10)
+number = random.randint(1, upper)
+
+print(f"I'm thinking of a number from 1 to {upper}. Can you guess it in {lives} tries?")
+guess = int(input(f"Your guess.. "))
 
 while(lives > 1 and guess != number):
     lives -= 1
@@ -13,7 +37,7 @@ while(lives > 1 and guess != number):
     guess = int(input())
 
 if guess == number:
+    lives -= 1
     print(f"You got it with {lives} tries to spare! Good job")
 else:
     print(f"Sorry, the number was {number}. Try again!")
-
